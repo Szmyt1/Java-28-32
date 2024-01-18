04ea590535ce093082a47741e9eb30c7a1183564
Szmyt1 <113186925+Szmyt1@users.noreply.github.com>
2024-01-18 19:33:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function zad28() {
+    window.open("zad_28_Szmyt.html", "okienko_z28","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function zad29() {
+    window.open("zad_29_Szmyt.html", "okienko_z29","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function zad30() {
+    window.open("zad_30_Szmyt.html", "okienko_z30","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function zad31() {
+    window.open("zad_31_Szmyt.html", "okienko_z31","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function zad32() {
+    window.open("zad_32_Szmyt.html", "okienko_z32","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ function zad30_kod() {
+    window.open("zad_30_Szmyt_kod.html", "okienko_z30_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+ }
+ 
+ function zad31_kod() {
+    window.open("zad_31_Szmyt_kod.html", "okienko_z31_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+ }
+ 
+ function zad32_kod() {
+    window.open("zad_32_Szmyt_kod.html", "okienko_z32_kod","toolbar=no, directories=no, menubar=no, height=720, width=800, top=350, left=600");
+ }
